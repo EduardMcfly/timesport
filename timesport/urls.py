7eb9c367cfede7def977bb1df37aec50a1541927
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-09-23 11:14:01 -0500

    refactor(urls.py): Imports are removed unused

--- timesport/urls.py
+++ timesport/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 ]
