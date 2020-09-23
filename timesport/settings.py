eaf8dc992b1f36eef56e69cca4cd5a47400833e6
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-09-23 11:08:20 -0500

    Updated to TEMPLATES setting.

--- timesport/settings.py
+++ timesport/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
 import os
 
@@ -17,7 +16,7 @@ import os
 PROJECT_PACKAGE = Path(__file__).resolve().parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = PROJECT_PACKAGE.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            str(PROJECT_PACKAGE.joinpath('templates'))
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -123,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
