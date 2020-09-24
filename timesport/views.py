4ba86ff6cd988b7beba77779da8b90f86331712c
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-09-24 17:32:11 -0500

    update(views.py): Added functions

--- timesport/views.py
+++ timesport/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def login(request):
     return render(request, 'login.html')
+
+def page404(request):
+    return JsonResponse({ 'error': 'Not exist' })
