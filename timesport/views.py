eb193c884e2c76d7ee50dd16daaa86a2bd6e974d
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-09-28 12:23:49 -0500

    chore: Typing is added to views

--- timesport/views.py
+++ timesport/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.handlers.wsgi import WSGIRequest
 
 
-def login(request):
+def login(request: WSGIRequest):
     return render(request, "login.html")
 
 
-def main(request):
+def main(request: WSGIRequest):
     return render(request, "main.html")
 
 
-def page404(request):
+def page404(request: WSGIRequest):
     return JsonResponse({"error": "Not exist"})
