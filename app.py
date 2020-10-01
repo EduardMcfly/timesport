c581941d73f068fe292217a11f17d3cf0259cf35
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-10-01 11:59:48 -0500

    chore: app.py added

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from jinja2.ext import Extension
+
+from utils import ext
+
+static_url_path = '/static'
+app = Flask(__name__, static_url_path=static_url_path)
+
+for extension in [ext.JinjaStatic, ext.JinjaUrl]:
+    app.jinja_env.add_extension(extension)
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/signUp")
+def signUp():
+    return render_template('signUp.html')
