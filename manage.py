94cd456a37e4b8e220c9e0e4667f4c01f560f9ef
Yeime.hut <yeimyangel29@gmail.com>
2020-09-30 21:35:26 -0500

    fix: syntax error solved

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""pip 
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timesport.settings')
     try:
         from django.core.management import execute_from_command_line
