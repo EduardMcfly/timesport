c9c0a6ee3a2b4b136e0a5776f6a0a894b7dfcc99
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-10-02 12:14:41 -0500

    chore: app.py has been updated

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
-from flask import Flask, render_template
-from jinja2.ext import Extension
-
+from flask import Flask, render_template, url_for, jsonify
 from utils import ext
+import os
+
+from database import getSession, db, migrate
+from models import *
 
 static_url_path = '/static'
 app = Flask(__name__, static_url_path=static_url_path)
 
 for extension in [ext.JinjaStatic, ext.JinjaUrl]:
+    # pylint: disable=no-member
     app.jinja_env.add_extension(extension)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'development' == app.env
+
+db.init_app(app)
+migrate.init_app(app)
+
+
+@app.context_processor
+def inject_dict_for_all_templates():
+    return dict(
+        static=lambda path: url_for('static', filename=path),
+        url=lambda path: url_for(path)
+    )
 
 
 @app.route("/")
 def index():
     return "Hello, World!"
+
+
+def query_to_dict(ret):
+    if ret is not None:
+        return [{key: value for key, value in row.items()} for row in ret if row is not None]
+    else:
+        return []
 
 
 @app.route("/login")
@@ -23,3 +47,12 @@ def login():
 @app.route("/signUp")
 def signUp():
     return render_template('signUp.html')
+
+
+@app.route("/users")
+def users():
+    session = getSession()
+    connection = session.connection()
+    results = connection.execute('SELECT * FROM "user"')
+    users = query_to_dict(results)
+    return jsonify(users)
