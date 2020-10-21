ed0606828a0994bc5a992cf387246d8647c8aae4
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-10-20 22:03:20 -0500

    chore: User model updated

--- models/User.py
+++ models/User.py
@@ -1,10 +1,15 @@
 from sqlalchemy.schema import Column
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import Integer, String
 
 from database import db
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
-    password = Column(String(128))
+    email = Column(String(128))
+    lastname = Column(String(128))
+    password = Column(String(128), nullable=False)
+    tracks = relationship("Track")
