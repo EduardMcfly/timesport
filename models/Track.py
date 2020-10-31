e350c4dac58c0c811914d06d8fe26bf08cf9d29c
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-10-31 00:59:26 -0500

    chore: The track model has been improved

--- models/Track.py
+++ models/Track.py
@@ -1,6 +1,7 @@
 from sqlalchemy.schema import Column
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.orm import relationship
 
 from database import db
 
@@ -12,3 +13,6 @@ class Track(db.Model):
     location = Column(String(128))
     size = Column(String(128))
     user_id = Column(Integer, ForeignKey('users.id'))
+    trainings = relationship("Training")
+    competences = relationship("Competence")
+
