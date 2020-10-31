dd877d331853c684332a5d9cd940697dda8e6bd9
Yineth.hut <castiblancoyinet13@gmail.com>
2020-10-31 17:26:09 -0500

    chore: create module homePage

---
+++ blueprints/homePage/__init__.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template, request, redirect
+from sqlalchemy.sql import text
+
+from database import db, getSession, migrate
+from models import *
+from utils import query_to_dict
+
+
+homePageBp = Blueprint(
+    'homePage',
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/homepage/static'
+)
+
+@homePageBp.route("/")
+def index():
+    return render_template('homePage.html')
