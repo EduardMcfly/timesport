3f9fb40f7f7cf2779b2f1b1f6f653f4fd7c10764
Yeime.hut <yeimyangel29@gmail.com>
2020-11-01 19:10:40 -0500

    chore: tracks file was modified

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from utils import ext, query_to_dict
 from blueprints.competence import competenceBp
 from login_manager import login_manager
 from blueprints.competence import competenceBp
-from blueprints.homePage import homePageBp
 
 
 static_url_path = '/static'
@@ -34,7 +33,6 @@ login_manager.init_app(app)
 app.register_blueprint(trainingBp)
 app.register_blueprint(authenticationBp)
 app.register_blueprint(trackBp)
-app.register_blueprint(homePageBp)
 
 
 @app.template_filter('date_format')
