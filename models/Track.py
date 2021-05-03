b766bf77b324903be1cf34547bd42299cdc797e3
nathsep9 <nathsep9@gmail.com>
2021-05-03 09:57:44 -0500

    fix: change was made in the type of data defined in track

--- models/Track.py
+++ models/Track.py
@@ -11,7 +11,7 @@ class Track(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
     location = Column(String(128))
-    size = Column(String(128))
+    size = Column(Integer())
     user_id = Column(Integer, ForeignKey('users.id'))
     trainings = relationship("Training")
     competences = relationship("Competence")
